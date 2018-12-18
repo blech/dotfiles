4fe17ebfab217390a9571fcb3e4db83c9b7fd0a4
Paul Mison <misonp@gmail.com>
2018-12-17 16:47:35 -0800

    Make tab complete case insensitive

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://askubuntu.com/questions/87061/can-i-make-tab-auto-completion-case-insensitive-in-the-terminal
+
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+echo 'set completion-ignore-case On' >> ~/.inputrc
+
